6acc5e6b0b26c15680c263b6dbd673ef1181ef85
Joseph <joseph.huang@epita.fr>
2020-07-26 23:11:55 +0200

    assigned outlets to collectionviewcell class

--- VelibApp/Views/StationCollectionViewCell.swift
+++ VelibApp/Views/StationCollectionViewCell.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class StationCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - Outlets
     
     @IBOutlet weak var stationNameLabel: UILabel!
     @IBOutlet weak var distanceToLabel: UILabel!
+    
+    @IBOutlet weak var nbEBikesLabel: UILabel!
+    @IBOutlet weak var nbBikesLabel: UILabel!
+    @IBOutlet weak var nbPlacesLabel: UILabel!
+    
+    // MARK: - Life cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,5 +33,9 @@ class StationCollectionViewCell: UICollectionViewCell {
     func configureWithStation(station: Station) {
         stationNameLabel.text = station.name
         distanceToLabel.text = "\(station.distance!) km"
+        
+        nbEBikesLabel.text = "\(station.eBike)"
+        nbBikesLabel.text = "\(station.mechanical)"
+        nbPlacesLabel.text = "\(station.numdocksavailable)"
     }
 }
